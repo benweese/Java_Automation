3ec347674ebf65dd7188dbeaa94ef0d132a50996
BenWeese <Ben.Weese@gmail.com>
2019-08-16 20:09:50 -0500

    Only headless on Circle CI runs.

--- src/test/java/StepDefinition/setup.java
+++ src/test/java/StepDefinition/setup.java
@@ -11,15 +11,15 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
     //Sets up the Driver
     void setDriver() {
+        ChromeOptions options = new ChromeOptions();
         //Sets the location of the chrome driver based on the repo
         if(System.getProperty("user.dir").equalsIgnoreCase("/home/circleci/repo")){
             System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/main/resources/chromedriverLinux");
+            options.addArguments("--headless");
         }else {
             System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/main/resources/chromedriver");
         }
         //This sets up the chrome browser with all options. Currently having issues with Headless in IntelliJ
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-gpu");
         options.addArguments("--window-size=1400x1200");
